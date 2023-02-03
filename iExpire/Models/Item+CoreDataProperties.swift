430985347baec39f35695655b99d14267c9b3f62
Andy Wu <awpats12@gmail.com>
2023-02-02 19:36:30 -0500

    Removed duplicate Item Core Data files

--- iExpire/Models/Item+CoreDataProperties.swift
+++ iExpire/Models/Item+CoreDataProperties.swift
@@ -16,13 +16,11 @@ extension Item {
         return NSFetchRequest<Item>(entityName: "Item")
     }
 
-    @NSManaged public var expirationDate: String?
-    @NSManaged public var image: Data?
     @NSManaged public var name: String?
+    @NSManaged public var expirationDate: String?
+    @NSManaged public var expirationDateTime: Date?
     @NSManaged public var notes: String?
-    @NSManaged public var expirationMonth: Int32
-    @NSManaged public var expirationDay: Int32
-    @NSManaged public var expirationYear: Int32
+    @NSManaged public var image: Data?
 
     public var wrappedName: String {
         name ?? "Unknown name"
@@ -36,15 +34,8 @@ extension Item {
         notes ?? ""
     }
     
-    public var expirationToDate: Date {
-        let dateNoCommas = expirationDate!.filter { $0 != "," }
-        
-        var components = DateComponents()
-        components.month = Int(expirationMonth)
-        components.day = Int(expirationDay)
-        components.year = Int(expirationYear)
-        
-        return Calendar.current.date(from: components)!
+    public var wrappedDateTime: Date {
+        expirationDateTime ?? Date.now
     }
 }
 
